330eee99d4f14240dade2c7489a1887e86fc2a05
james russell <jamesrussell@pursuit.org>
2021-11-19 11:58:27 -0500

    mapped through products to display on screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import { Component } from "react";
 import "./App.css";
+import data from "./data/productData"
 
-const App = () => {
-  return <h1>Hello, world!</h1>;
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      productsList: data
+    }
+  }
+
+  handleButtonClick = (product) => {
+
+  }
+
+  render() {
+
+    let productsArr = this.state.productsList.map((product) => {
+      let { name, price, image, description} = product
+      return(
+        <div className="product-info-container" onClick={()=>this.handleButtonClick(product)}>
+          <div>Name: { name }</div>
+          <div>Price: { price }</div>
+          <button>Add To Cart</button>
+          <img src={image} alt="product image"/>
+          <div>Description: { description }</div>
+        </div>
+      )
+    })
+    return(
+      <div>
+        <h2>My Garage Sale</h2>
+
+        <div id="product-list-container">{ productsArr }</div>
+
+      </div>
+    )
+
+  }
 };
 
 export default App;
